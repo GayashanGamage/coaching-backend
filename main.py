87de778716b7598df54fe66568c9a7b9ed185b9e
gayashan <gayashan.randimagamage@gmail.com>
2024-07-11 16:46:13 +0530

    add CORS details

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 from fastapi import FastAPI
 from Backoffice import Backoffice
 from Client import Client
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(Backoffice.routes)
 app.include_router(Client.routes)
+
+from fastapi import FastAPI

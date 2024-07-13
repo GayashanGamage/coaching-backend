8da16090dc7a7c6d8378d8a1d3a937225ac45b85
gayashan <gayashan.randimagamage@gmail.com>
2024-07-14 05:12:58 +0530

    add register endpoint

--- Client/Schemas.py
+++ Client/Schemas.py
@@ -1,1 +1,29 @@
-# this is for define for schemas 
+# this is for define for schemas
+from pydantic import BaseModel, EmailStr
+
+
+class User(BaseModel):
+    fistName: str
+    lastName: str
+    password: str
+    email: EmailStr
+
+
+class UserAuth(User):
+    password: str
+
+
+class UserEmail(BaseModel):
+    email: EmailStr
+
+
+class UserVerification(UserEmail):
+    password: str
+
+
+class VerificationCode(UserEmail):
+    code: int
+
+
+class PasswordReset(UserVerification):
+    pass
--- Client/User.py
+++ Client/User.py
@@ -1,26 +1,64 @@
-# this is for all user account related things 
+# this is for all user account related things
 # create new account and authonticate
 
 from fastapi import APIRouter
+from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+from .Schemas import UserAuth, User, UserEmail, VerificationCode, PasswordReset
+from fastapi.responses import JSONResponse
+from Auth import *
+from random import randint
+from datetime import datetime
+import sib_api_v3_sdk
+from sib_api_v3_sdk.rest import ApiException
+
+# brevo configuration
+configuration = sib_api_v3_sdk.Configuration()
+configuration.api_key['api-key'] = os.getenv('brevo')
+api_instance = sib_api_v3_sdk.TransactionalEmailsApi(
+    sib_api_v3_sdk.ApiClient(configuration))
+
+# mongodb configuration
+load_dotenv()
+mongo = MongoClient(os.getenv('mongo'))
+db = mongo['consult']
+userdb = db['user']
 
 auth = APIRouter()
 
 # login for user
+
+
 @auth.post('/login', tags=['client-user'])
 async def login():
     pass
 
 # change passwrod
+
+
 @auth.post('/password-reset', tags=['client-user'])
 async def passwordReset():
     pass
 
-# get verification code for 
+# get verification code for
+
+
 @auth.post('/verification-code', tags=['client-user'])
 async def verificationCode():
     pass
 
 # create a new use account
+
+
 @auth.post('/register', tags=['client-user'])
-async def register():
-    pass
+async def register(user: User):
+    userDetails = userdb.find_one({"email": user.email})
+    if userDetails == None:
+        hash_password = hashPassword(user.password)
+        userdb.insert_one({"email": user.email, "fistName": user.fistName,
+                          "lastName": user.lastName, "passwrod": hash_password})
+        return JSONResponse(status_code=201, content={"message": "sucsessfull"})
+    else:
+        print(userDetails)
+        return JSONResponse(status_code=400, content={"error": "email exist"})

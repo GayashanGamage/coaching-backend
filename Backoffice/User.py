8dd4b35ae136ee69176b73d2ca4fcb81535122b3
gayashan <gayashan.randimagamage@gmail.com>
2024-07-14 05:31:59 +0530

    relocate email functionality to new file

--- Backoffice/User.py
+++ Backoffice/User.py
@@ -9,14 +9,8 @@ from fastapi.responses import JSONResponse
 from Auth import *
 from random import randint
 from datetime import datetime
-import sib_api_v3_sdk
-from sib_api_v3_sdk.rest import ApiException
+from Email import *
 
-# brevo configuration
-configuration = sib_api_v3_sdk.Configuration()
-configuration.api_key['api-key'] = os.getenv('brevo')
-api_instance = sib_api_v3_sdk.TransactionalEmailsApi(
-    sib_api_v3_sdk.ApiClient(configuration))
 
 load_dotenv()
 mongo = MongoClient(os.getenv('mongo'))
@@ -26,28 +20,10 @@ admin = db['admin']
 auth = APIRouter()
 
 # ------------------------------
-# seperated functions
-# send email
-
-
-def sendEmail(mailData):
-    print(mailData)
-    Sender = {"name": "gayashan", "email": "gayashan.randimagamage@gmail.com"}
-    Headers = {"Some-Custom-Name": "unique-id-1234"}
-    send_smtp_email = sib_api_v3_sdk.SendSmtpEmail(
-        to=mailData['to'], headers=Headers, sender=Sender, subject=mailData['subject'], params=mailData['params'], template_id=mailData['template'])
-    send_smtp_email = sib_api_v3_sdk.SendSmtpEmail(to=[{"name": "chamodi", "email": "chamodijanithya@gmail.com"}], headers={"Some-Custom-Name": "unique-id-1234"}, sender={
-                                                   "name": "gayashan", "email": "gayashan.randimagamage@gmail.com"}, subject="password reset", template_id=3, params=mailData["params"])
-    try:
-        api_response = api_instance.send_transac_email(send_smtp_email)
-        return {'mail_id': api_response._message_id, 'message': 'sucsusfull'}
-    except ApiException as e:
-        return {'mail_id': None, 'message': 'Unsucsusfull'}
-
-
-# ------------------------------
 # endpoints
 # user login - admin
+
+
 @auth.post('/admin-login', tags=['backoffice-user'])
 async def login(user: UserAuth):
     userDetails = admin.find_one({'username': user.username})
---
+++ Email.py
@@ -0,0 +1,27 @@
+import sib_api_v3_sdk
+from sib_api_v3_sdk.rest import ApiException
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# brevo configuration
+configuration = sib_api_v3_sdk.Configuration()
+configuration.api_key['api-key'] = os.getenv('brevo')
+api_instance = sib_api_v3_sdk.TransactionalEmailsApi(
+    sib_api_v3_sdk.ApiClient(configuration))
+
+
+def sendEmail(mailData):
+    print(mailData)
+    Sender = {"name": "gayashan", "email": "gayashan.randimagamage@gmail.com"}
+    Headers = {"Some-Custom-Name": "unique-id-1234"}
+    send_smtp_email = sib_api_v3_sdk.SendSmtpEmail(
+        to=mailData['to'], headers=Headers, sender=Sender, subject=mailData['subject'], params=mailData['params'], template_id=mailData['template'])
+    send_smtp_email = sib_api_v3_sdk.SendSmtpEmail(to=[{"name": "chamodi", "email": "chamodijanithya@gmail.com"}], headers={"Some-Custom-Name": "unique-id-1234"}, sender={
+                                                   "name": "gayashan", "email": "gayashan.randimagamage@gmail.com"}, subject="password reset", template_id=3, params=mailData["params"])
+    try:
+        api_response = api_instance.send_transac_email(send_smtp_email)
+        return {'mail_id': api_response._message_id, 'message': 'sucsusfull'}
+    except ApiException as e:
+        return {'mail_id': None, 'message': 'Unsucsusfull'}

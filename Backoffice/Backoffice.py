5a0a334b6a08ad057fd0eec0a01f859a378a830a
gayashan <gayashan.randimagamage@gmail.com>
2024-07-17 00:59:06 +0530

    add live session functionalities

--- Backoffice/Backoffice.py
+++ Backoffice/Backoffice.py
@@ -3,23 +3,50 @@
 from fastapi import APIRouter
 from . import User
 from pymongo import MongoClient
+from .Schemas import LiveSession, LiveSessionWithId
+from fastapi.responses import JSONResponse
+import os
+from dotenv import load_dotenv
+from bson import ObjectId
 
-mongo = MongoClient('mongodb+srv://gayashangamage:2692g.rg0968CJ@consultservice.c3gvek2.mongodb.net/?retryWrites=true&w=majority&appName=consultService')
+load_dotenv()
+
+mongo = MongoClient(os.getenv('mongo'))
 db = mongo['consult']
 cluster = db['users']
+liveSessionSchedul = db['liveSessionSchedul']
 
 routes = APIRouter()
 routes.include_router(User.auth)
 
 # create new live session
 @routes.post('/set-live-session', tags=['backoffice-function'])
-async def setLiveSession(): 
-    pass
+async def setLiveSession(details : LiveSession):
+    allSession = liveSessionSchedul.find_one({'date' : details.date})
+    
+    # find sameday sessions 
+    if allSession != None:
+        return JSONResponse(status_code=400, content={"message" : "cannot post two sessions in one day"})
+    else:
+        sessionData = details.model_dump()
+        liveSessionSchedul.insert_one(sessionData)
+        return JSONResponse(status_code=200, content={"message" : "sucsesfull"})
 
-# 
+# update live session details
+@routes.patch('/update-live-session', tags=['backoffice-function'])
+async def updateLiveSession(details : LiveSessionWithId):
+    session = liveSessionSchedul.find_one({'_id' : ObjectId(details.id)})
+    if session == None:
+        return JSONResponse(status_code=400, content={"error" : 'content not found'})
+    else:
+        updatedDetails = details.model_dump(exclude={'_id', 'create'})
+        liveSessionSchedul.update_one({'_id' : ObjectId(details.id) }, {'$set' : updatedDetails})
+        return JSONResponse(status_code=200, content={'message' : 'sucsessful'})
+
 @routes.get('/list-session', tags=['backoffice-function'])
 async def listSession():
     pass
+
 
 # manage admins' timeslots 
 @routes.post('/manage-timeslots', tags=['backoffice-function'])
--- Backoffice/Schemas.py
+++ Backoffice/Schemas.py
@@ -1,6 +1,10 @@
 # this is for define all schemass
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, Field
+from datetime import datetime
+from typing import Optional
 
+date = str(datetime.now().date())
+time = str(datetime.now().time())
 
 class User(BaseModel):
     username: str
@@ -24,3 +28,17 @@ class VerificationCode(UserEmail):
 
 class PasswordReset(UserVerification):
     pass
+
+class LiveSession(BaseModel):
+    title : str
+    description : str
+    agenda : list
+    venue : str
+    date : str = Field(default=date)
+    time : str = Field(default=time)
+    postTime : datetime
+    create : Optional[datetime] = Field(default=datetime.now())
+
+class LiveSessionWithId(LiveSession):
+    id : str
+    lastUpdate : Optional[datetime] = Field(default=datetime.now())
